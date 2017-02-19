8ff96bb1d4ea509b1597968bebf4950e9ebc0144
Three Fat Kin <ThreeFat@users.noreply.github.com>
2017-02-19 09:27:21 +0800

    Add files via upload

---
+++ Chapter7/common.js
@@ -0,0 +1,19 @@
+/**
+ * Created by jf on 2016/8/18.
+ */
+
+
+/**
+ * 封装 获取当前元素的第一个子元素
+ * @param element
+ * @returns {*}
+ */
+
+
+
+/**
+ * 封装 获取当前元素的最后一个子元素
+ * @param element
+ * @returns {*}
+ */
+
